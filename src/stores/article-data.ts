cb22f056b0021c45d87a7136007e4c4881679d03
EthanSk13s <ethanmiranda11@gmail.com>
2025-06-09 23:52:25 -0700

    remove debug print

--- src/stores/article-data.ts
+++ src/stores/article-data.ts
@@ -14,7 +14,6 @@ export const ArticleData = defineStore('article-data', {
     actions: {
         async init() {
             this.articles = await getArticles();
-            console.log(this.articles);
         }
     }
 })
