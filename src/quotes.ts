e129868d1a9a3e1cdad9b307c59176e65b07613f
EthanSk13s <ethanmiranda11@gmail.com>
2025-06-11 17:47:35 -0700

    add quote

--- src/quotes.ts
+++ src/quotes.ts
@@ -13,5 +13,10 @@ export const QUOTES: Quote[] = [
     { origin: 'Sing a Wing Song, Tanaka Kotoha', 
       text: `There are days when you can't do it perfectly (it's alright!)
        But there's nothing wrong with trying.`,
-      url: 'https://www.youtube.com/watch?v=BdDOw5bJI6A' }
+      url: 'https://www.youtube.com/watch?v=BdDOw5bJI6A' },
+    { origin: 'S(mile)ing, Shimamura Uzuki',
+      text: `Rise! I won't give up anymore.
+       I promise myself of yesterday who
+       was in bed down in the dumps.`,
+      url: 'https://www.youtube.com/watch?v=aQr0szotP_Y' }
 ]
