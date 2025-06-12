e3aaf054fea5eab558fb0c67dab654c67660b8a6
EthanSk13s <ethanmiranda11@gmail.com>
2025-06-11 18:51:22 -0700

    remove base url

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ import NotFoundView from '@/views/NotFoundView.vue';
 import ProjectsView from '@/views/ProjectsView.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
