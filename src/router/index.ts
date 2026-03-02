5f5c9c3452026162fb2ccb422e261c27b820c422
EthanSk13s <ethanmiranda11@gmail.com>
2026-03-01 21:36:53 -0800

    add parent calc to routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@ import NotFoundView from '@/views/NotFoundView.vue';
 import ProjectsView from '@/views/ProjectsView.vue';
 import BlogsView from '@/views/BlogsView.vue';
 import ToolsView from '@/views/tools/ToolsView.vue';
+import UmaParentCalcView from '@/views/tools/uma-parent-calc/UmaParentCalcView.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -39,6 +40,11 @@ const router = createRouter({
       path: '/tools',
       name: 'tools',
       component: ToolsView
+    },
+    {
+      path: '/tools/uma-parent-calc',
+      name: 'uma-parent-calc',
+      component: UmaParentCalcView
     }
   ],
 })
@@ -52,6 +58,10 @@ router.afterEach((to, from) => {
     to.meta.title = "Page not found"; 
   } else if (to.name === 'Projects') {
     to.meta.title = "Projects";
+  } else if (to.name === 'uma-parent-calc') {
+    to.meta.title = "Umamusume Parent Calculator"
+  } else if (to.name === 'tools') {
+    to.meta.title = "Tools"
   }
 
   document.title = "EthanSk13s - " + to.meta.title;
