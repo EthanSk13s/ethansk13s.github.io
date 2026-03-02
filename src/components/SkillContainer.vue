fcd046163867012626d5c8adb664fd9a08a74cf5
EthanSk13s <ethanmiranda11@gmail.com>
2026-03-01 20:40:56 -0800

    update svg

--- src/components/SkillContainer.vue
+++ src/components/SkillContainer.vue
@@ -39,14 +39,8 @@ function updateSkillType(option: any) {
       p-1 bg-ctp-red rounded-full shadow-sm shadow-ctp-red
       hover:shadow-md text-ctp-base"
     >
-      <!-- License: PD. Made by Arthur Kazais: https://www.figma.com/@thearthurk -->
-      <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
-        <path d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929
-          5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658
-          18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166
-          19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976
-          5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237
-          6.31658 4.90237 5.68342 5.29289 5.29289Z"/>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" class="size-4">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
       </svg>
     </button>
     <div class="col-start-2 row-start-2">
